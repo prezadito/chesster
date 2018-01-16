e1e2845ca07b22c59899d8a98cc57847b31cbf58
Aditya Niloi <aditya4mvp@gmail.com>
2018-01-15 19:36:54 -0500

    board position

--- js/main.js
+++ js/main.js
@@ -1,42 +1,10 @@
-var board = ChessBoard('board', 'start');
-
 var board,
   game = new Chess(),
   statusEl = $('#status'),
   fenEl = $('#fen'),
   pgnEl = $('#pgn');
 
-// do not pick up pieces if the game is over
-// only pick up pieces for the side to move
-var onDragStart = function(source, piece, position, orientation) {
-  if (game.game_over() === true ||
-      (game.turn() === 'w' && piece.search(/^b/) !== -1) ||
-      (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
-    return false;
-  }
-};
-
-var onDrop = function(source, target) {
-  // see if the move is legal
-  var move = game.move({
-    from: source,
-    to: target,
-    promotion: 'q' // NOTE: always promote to a queen for example simplicity
-  });
-
-  // illegal move
-  if (move === null) return 'snapback';
-
-  updateStatus();
-};
-
-// update the board position after the piece snap 
-// for castling, en passant, pawn promotion
-var onSnapEnd = function() {
-  board.position(game.fen());
-};
-
-var updateStatus = function() {
+/* var updateStatus = function() {
   var status = '';
 
   var moveColor = 'White';
@@ -67,16 +35,58 @@ var updateStatus = function() {
   statusEl.html(status);
   fenEl.html(game.fen());
   pgnEl.html(game.pgn());
-};
+}; */
 
 var cfg = {
   draggable: true,
   position: '3qk3/8/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq',
   onDragStart: onDragStart,
   onDrop: onDrop,
-//   onSnapEnd: onSnapEnd,
+  onSnapEnd: onSnapEnd,
   orientation: 'black'
 };
+
 board = ChessBoard('board', cfg);
+
+// do not pick up pieces if the game is over
+// only pick up pieces for White
+var onDragStart = function(source, piece, position, orientation) {
+    if (game.in_checkmate() === true || game.in_draw() === true ||
+      piece.search(/^b/) !== -1) {
+      return false;
+    }
+  };
+  
+  var makeRandomMove = function() {
+    var possibleMoves = game.moves();
+  
+    // game over
+    if (possibleMoves.length === 0) return;
+  
+    var randomIndex = Math.floor(Math.random() * possibleMoves.length);
+    game.move(possibleMoves[randomIndex]);
+    board.position(game.fen());
+  };
+  
+  var onDrop = function(source, target) {
+    // see if the move is legal
+    var move = game.move({
+      from: source,
+      to: target,
+      promotion: 'q' // NOTE: always promote to a queen for example simplicity
+    });
+  
+    // illegal move
+    if (move === null) return 'snapback';
+  
+    // make random legal move for black
+    window.setTimeout(makeRandomMove, 250);
+  };
+  
+  // update the board position after the piece snap
+  // for castling, en passant, pawn promotion
+  var onSnapEnd = function() {
+    board.position(game.fen());
+  };
 
 updateStatus();
